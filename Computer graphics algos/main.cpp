bd9467596eaf4721cf91918e8cec72c332b6e1b5
snorm <snorm@MSI>
2025-10-22 15:23:31 -0500

    Added SDL to project for real time graphics and mouse/keyboard input.
    This introduces lots of abstraction into this project, but it'll be  good practice with a (possibly) simpler API than DirectX11. At any rate, I don't think I'll have to worry about all the jargon and boilerplate code of graphics pipelines.
    
    NOTE: the SDL file is NOT included in this repo - to get this working, follow the instructions at:
    
    https://github.com/libsdl-org/SDL/blob/main/docs/INTRO-visualstudio.md

--- Computer graphics algos/main.cpp
+++ Computer graphics algos/main.cpp
@@ -1,89 +1,89 @@
-// Computer graphics algos.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//// Computer graphics algos.cpp : This file contains the 'main' function. Program execution begins and ends there.
+////
+//#define _USE_MATH_DEFINES
 //
-#define _USE_MATH_DEFINES
-
-#include<array> 
-#include<chrono>
-#include<fstream> 
-#include<iostream>
-#include<thread> 
-#include<vector>
-
-#include"Edge.h"
-#include"GraphicsDemo.h"
-#include"ImageBMP.h"
-#include "MyException.h"
-#include"Polygon.h"
-#include"Triangle.h"
-#include"UnorganizedDemos.h"
-#include"Utils.h"
-#include "Vec4.h"
-#include "Mat4.h"
-//#include "CoordinateTransformer.h"
-#include "Cube.h"
-#include "Camera.h"
-#include "Mesh.h"
-
-
-
-int main()
-{
-	try
-	{
-		constexpr int screenWidth = 500;
-		constexpr int screenHeight = 500;
-
-
-		Cube cube{};
-
-		std::vector<Vec4> localCubeVerts = cube.getLocalCubeVerts();
-
-		int loopCount = 0; 
-
-		while (true)
-		{
-			/*Construct a Mesh that (possibly) applies transformations (rot, scale, trans) on the cube
-			* - this converts from "local" to "world" coordinates
-			*/
-			Vec4 rot(M_PI/6.0f*loopCount, M_PI/5.0f * loopCount, M_PI / 4.0f * loopCount, 0.0f); //units: radians, rotate about y-axis 45 degrees (w = 0.0f here?) 
-			Vec4 scale(1.0f, 1.0f, 1.0f, 0.0f); //again, not sure if w = 0.0f or 1.0f here 
-			Vec4 trans(0.0f, 0.0f, -3.0f, 1.0f); //ensure that z < 0.0f  (default camera pos) for zInit + zTrans
-
-			Mesh mesh(localCubeVerts, trans, rot, scale);
-
-			std::vector<Vec4> worldCubeVerts = mesh.applyModelMatrix(); 
-
-			float fovY = (M_PI / 2);
-			Camera camera(fovY);
-
-			auto screenSpaceCubeVerts = camera.projectToScreen(worldCubeVerts, screenWidth, screenHeight);
-
-			std::vector<Color> colors; 
-			auto colorEnums = Color::getGrayColorPalette(); 
-			for (const auto& current : colorEnums)
-			{
-				colors.push_back(Color(current));
-			}
-			//auto rasterPoints = cube.rasterize(screenSpaceCubeVerts);
-			//auto rasteredPixels = cube.rasterize(screenSpaceCubeVerts, colors);
-			auto rasteredPixels = cube.filledRasterize(screenSpaceCubeVerts, colors);
-
-			ImageBMP image(screenWidth, screenHeight, ColorEnum::Pink);
-
-			//image.fillPixelMatrix(rasteredPoints, ColorEnum::Purple);
-
-			image.fillPixelMatrix(rasteredPixels);
-
-			image.saveAsPNG("TETRAHEDRON_" + Utils::getTimestampForFilename() + ".png");
-			//camera.logCameraInfo("CameraLog" + Utils::getTimestampForFilename() + ".log");	
-			 
-			loopCount++; 
-		}	
-	}
-
-	catch (const MyException& e)
-	{
-		std::cout << e.whatWentWrong() << "\n";
-	}
-}
-
+//#include<array> 
+//#include<chrono>
+//#include<fstream> 
+//#include<iostream>
+//#include<thread> 
+//#include<vector>
+//
+//#include"Edge.h"
+//#include"GraphicsDemo.h"
+//#include"ImageBMP.h"
+//#include "MyException.h"
+//#include"Polygon.h"
+//#include"Triangle.h"
+//#include"UnorganizedDemos.h"
+//#include"Utils.h"
+//#include "Vec4.h"
+//#include "Mat4.h"
+//#include "Cube.h"
+//#include "Camera.h"
+//#include "Mesh.h"
+//
+//
+//
+//
+//int main()
+//{
+//	try
+//	{
+//		constexpr int screenWidth = 500;
+//		constexpr int screenHeight = 500;
+//
+//
+//		Cube cube{};
+//
+//		std::vector<Vec4> localCubeVerts = cube.getLocalCubeVerts();
+//
+//		int loopCount = 0; 
+//
+//		while (true)
+//		{
+//			/*Construct a Mesh that (possibly) applies transformations (rot, scale, trans) on the cube
+//			* - this converts from "local" to "world" coordinates
+//			*/
+//			Vec4 rot(M_PI/6.0f*loopCount, M_PI/5.0f * loopCount, M_PI / 4.0f * loopCount, 0.0f); //units: radians, rotate about y-axis 45 degrees (w = 0.0f here?) 
+//			Vec4 scale(1.0f, 1.0f, 1.0f, 0.0f); //again, not sure if w = 0.0f or 1.0f here 
+//			Vec4 trans(0.0f, 0.0f, -3.0f, 1.0f); //ensure that z < 0.0f  (default camera pos) for zInit + zTrans
+//
+//			Mesh mesh(localCubeVerts, trans, rot, scale);
+//
+//			std::vector<Vec4> worldCubeVerts = mesh.applyModelMatrix(); 
+//
+//			float fovY = (M_PI / 2);
+//			Camera camera(fovY);
+//
+//			auto screenSpaceCubeVerts = camera.projectToScreen(worldCubeVerts, screenWidth, screenHeight);
+//
+//			std::vector<Color> colors; 
+//			auto colorEnums = Color::getGrayColorPalette(); 
+//			for (const auto& current : colorEnums)
+//			{
+//				colors.push_back(Color(current));
+//			}
+//			//auto rasterPoints = cube.rasterize(screenSpaceCubeVerts);
+//			//auto rasteredPixels = cube.rasterize(screenSpaceCubeVerts, colors);
+//			auto rasteredPixels = cube.filledRasterize(screenSpaceCubeVerts, colors);
+//
+//			ImageBMP image(screenWidth, screenHeight, ColorEnum::Pink);
+//
+//			//image.fillPixelMatrix(rasteredPoints, ColorEnum::Purple);
+//
+//			image.fillPixelMatrix(rasteredPixels);
+//
+//			image.saveAsPNG("TETRAHEDRON_" + Utils::getTimestampForFilename() + ".png");
+//			//camera.logCameraInfo("CameraLog" + Utils::getTimestampForFilename() + ".log");	
+//			 
+//			loopCount++; 
+//		}	
+//	}
+//
+//	catch (const MyException& e)
+//	{
+//		std::cout << e.whatWentWrong() << "\n";
+//	}
+//}
+//
